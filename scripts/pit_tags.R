19bf6f3a223aab2804d10faf6f0e2036e612423f
almac2022 <al@newgraphenvironment.com>
2022-09-23 12:28:47 -0700

    join and burn

--- scripts/pit_tags.R
+++ scripts/pit_tags.R
@@ -1,26 +1,52 @@
 #combining pit tag data to individual fish data so that we can copy and paste directly into submission template
+# would be good to include the comments and times/camera of photos in the fish csv and then paste them to the right of the pit tag.
 
 source('scripts/packages.R')
 
 #import the pit tag csv
-path <- '../../repo/dff-2022/data/pars/tag_01_01.csv'
+# because we are working in a project our working directory is the root folder so all paths are
+# relative to that
+# path <- '../../repo/dff-2022/data/pars/tag_01_01.csv'
+path <- 'data/pars/tag_01_01.csv'
 
-pit_tag <- read.csv(path)
+# using readr is better than read.csv
+pit_tag <- readr::read_csv(path) %>%
+  #separate the pit tag out from the rest of the info in the pit tag csv
+  # https://stackoverflow.com/questions/66696779/separate-by-pattern-word-in-tidyr-and-dplyr
+  tidyr::separate(col=tag, into=c('date', 'tag_id'), sep='\\s*TAG\\s*') %>%
+  tibble::rowid_to_column()
 
-#separate the pit tag out from the rest of the info in the pit tag csv
-tags <- tidyr::separate(pit_tag, col=tag, into=c('date', 'tag_id'), sep=27)
-
-#generate row numbers
-rows <- dplyr::mutate(tags, id= row_number())
 
 #import csv with fish data
-path2 <- '../../repo/dff-2022/data/pars/fish_data.csv'
+path2 <- 'data/pars/fish_data.csv'
 
-fish <- read.csv(path2)
+fish <- readr::read_csv(path2)
 
 #join fish csv with pit tag csv based on tag row number
-dplyr::left_join(tags, fish, by=c("tag_row"="rows"))
+fish_tags <- dplyr::left_join(fish,
+                              pit_tag,
+                              by = c("tag_row" = "rowid")) %>%
+  # arrange columns
+  relocate(tag_id, .after = last_col()) %>%
+  # remove the first row because it was from the office. We need to pass the object piped to nrow as a "."
+  # as the first object gets passed to the slice function only
+  dplyr::slice(2:nrow(.)) %>%
+  # add a period, a space and the row number to the pit tag to go in the comments to make it easy to pull anything out we want later
+  dplyr::mutate(tag_id = case_when(
+    !is.na(tag_id) ~ paste0(tag_id, '. Row ID ', tag_row, '. '),
+    T ~ tag_id))
 
+# select a subsample of fish (lets go 15% since the sample size is small) to review manually to be sure the
+# pit tags match which fish they go with
+# set seed for reproducible sample - try running it again without setting the seed immediately before and see how it differs
+set.seed(1234)
+sample(nrow(fish_tags), nrow(fish_tags) * 0.15) %>%
+  # arrange the row numbers to make it easier to QA
+  sort()
 
+# burn the csv ready to cut and paste
+fish_tags %>%
+  readr::write_csv('data/pars/fish_tags_joined.csv',
+                   na = "" )
 
 
