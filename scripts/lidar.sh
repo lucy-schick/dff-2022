d9bf890c6acc09985059e1ff9cfad4fc94cf5333
almac2022 <al@newgraphenvironment.com>
2023-03-16 08:52:52 -0700

    add base dir as variable code even though not used

--- scripts/lidar.sh
+++ scripts/lidar.sh
@@ -5,6 +5,10 @@ set -euxo pipefail
 # start the timer
 SECONDS=0
 
+#As long as we run the sh script from the main directory we do not need to mess with the basedir.  have left here as it will likely come in handy
+# later
+BASEDIR="${PWD%/*}"
+
 mkdir -p data/lidar
 
 wget --trust-server-names -qN https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/bc_093l056_xli1m_utm09_2019.tif --directory-prefix=data/lidar
