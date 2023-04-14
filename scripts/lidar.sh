98c12d1b25b3525da3f5c2d0d990bb08342f5d26
almac2022 <al@newgraphenvironment.com>
2023-04-13 21:15:30 -0700

    parameterize filename and sync to aws bucket

--- scripts/lidar.sh
+++ scripts/lidar.sh
@@ -5,16 +5,24 @@ set -euxo pipefail
 # start the timer
 SECONDS=0
 
+FILENAME="20210906lampreymoricetribv220230317-DEM.tif"
+
 #As long as we run the sh script from the main directory we do not need to mess with the basedir.  have left here as it will likely come in handy
 # later
 BASEDIR="${PWD%/*}"
 
 mkdir -p data/lidar
 
-wget --trust-server-names -qN https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/bc_093l056_xli1m_utm09_2019.tif --directory-prefix=data/lidar
+# wget --trust-server-names -qN https://nrs.objectstore.gov.bc.ca/gdwuts/093/093l/2019/dem/$FILENAME --directory-prefix=data/lidar
 
 # convert to cog
-rio cogeo create data/lidar/bc_093l056_xli1m_utm09_2019.tif data/lidar/bc_093l056_xli1m_utm09_2019_cog.tif
+rio cogeo create data/lidar/$FILENAME data/lidar/$FILENAME
+
+# remove the orginal file
+# rm data/lidar/bc_093l056_xli1m_utm09_2019.tif
+
+# https://aws.amazon.com/cli/
+aws s3 sync data/lidar/ s3://23cog
 
 echo 'This took' \
 $SECONDS \
