fd6abda863ef8996008041d9d9f67542f57d70f7
almac2022 <al@newgraphenvironment.com>
2023-06-02 09:24:40 -0700

    update with new project name

--- scripts/qgis/mergin_make_project.sh
+++ scripts/qgis/mergin_make_project.sh
@@ -5,19 +5,23 @@ set -euxo pipefail
 # create project
 # ---------------
 # with Mergin Maps authentication token string set as --auth-token env variable run
-mergin create newgraph/skeena_2023 --from-dir ~/Projects/gis/skeena_2023
-
+mergin create newgraph/sern_skeena_2023 --from-dir ~/Projects/gis/sern_skeena_2023
 
 # ---------------
 # invite collaborators
 # ---------------
 # executed from project directory with .mergin file present
-mergin share-add newgraph/skeena_2023 newgraph_bute --permissions writer
-
+mergin share-add newgraph/sern_skeena_2023 newgraph_bute --permissions writer
+mergin share-add newgraph/sern_skeena_2023 MateoW --permissions writer
+mergin share-add newgraph/sern_skeena_2023 snorris --permissions writer
 
 # ---------------
 # sync changes
 # ---------------
 # executed from project directory with .mergin file present
-mergin status newgraph/skeena_2023
-mergin push newgraph/skeena_2023
+mergin status newgraph/sern_skeena_2023
+mergin push newgraph/sern_skeena_2023
+
+# ---------------
+# clone a project
+# ---------------
