45ee272ca894cb3ab77e0e630a54329495183dda
Mateo Winterscheidt <matwint45@gmail.com>
2023-08-10 12:57:53 -0700

    commented out code for other fish site form, using fiss form instead

--- scripts/fish_sample_build.R
+++ scripts/fish_sample_build.R
@@ -25,59 +25,59 @@ fish_coll <- fpr::fpr_import_hab_con(backup = F, row_empty_remove = T) %>%
   select(local_name, species:weight_g, comments) %>%
   slice(1)
 
-# pull out site info from step 2
-fish_site <- fpr::fpr_import_hab_con(backup = F, row_empty_remove = T) %>%
-  pluck('step_2_fish_coll_data') %>%
-  select(gazetted_name, local_name, temperature_c:turbidity, sampling_method, ef_seconds:frequency) %>%
-  slice(1)
+# # pull out site info from step 2
+# fish_site <- fpr::fpr_import_hab_con(backup = F, row_empty_remove = T) %>%
+#   pluck('step_2_fish_coll_data') %>%
+#   select(gazetted_name, local_name, temperature_c:turbidity, sampling_method, ef_seconds:frequency) %>%
+#   slice(1)
 
 # see the names of the columns
 names(fish_coll)
-names(fish_site)
+#names(fish_site)
 
 #' name the forms using the date and time
 #' we should be able to name the forms the same in the active project but the files can be versioned
 
-file_name_site <- paste0('form_fish_site_', format(lubridate::now(), "%Y%m%d%H%m"))
+#file_name_site <- paste0('form_fish_site_', format(lubridate::now(), "%Y%m%d%H%m"))
 file_name_fish <- paste0('form_fish_sample_', format(lubridate::now(), "%Y%m%d%H%m"))
 
-form_prep1 <- fish_site %>%
-  # add some columns of our own
-  dplyr::mutate(date_time_start = NA_POSIXct_,
-                utm_easting = NA_integer_,
-                utm_northing = NA_integer_,
-                mergin_user = NA_character_,
-                surveyor_1 = NA_character_,
-                surveyor_2 = NA_character_,
-                surveyor_3 = NA_character_,
-                camera_id = NA_character_,
-                gps_id = NA_character_,
-                gps_waypoint_number = NA_character_,
-                photo_site_bottom = NA_character_,
-                photo_site_top = NA_character_
-  ) %>%
-  # populate utm fields with a location so we can use to burn geopackage
-  mutate(utm_easting = 687879,
-         utm_northing = 6020659) %>%
-  # make it a spatial file so we can burn it as a geopackage right into our mergin file of choice
-  sf::st_as_sf(coords = c("utm_easting", "utm_northing"),
-               crs = 32600 + utm_zone, remove = F) %>%
-  # put it into albers so we can use it anywhere
-  sf::st_transform(crs = 3005) %>%
-  relocate(date_time_start,
-           mergin_user,
-           contains('surveyor'),
-           contains('name'),
-           matches('utm')) %>%
-  relocate(matches('photo'), .after = last_col()) %>%
-  # burn to test project for now, will burn to dff when finalised
-  sf::st_write(paste0('../../gis/',
-                      dir_project,
-                      '/',
-                      file_name_site,
-                      '.gpkg'),
-               # turned this T now that we have time in name
-               delete_layer = T)
+# form_prep1 <- fish_site %>%
+#   # add some columns of our own
+#   dplyr::mutate(date_time_start = NA_POSIXct_,
+#                 utm_easting = NA_integer_,
+#                 utm_northing = NA_integer_,
+#                 mergin_user = NA_character_,
+#                 surveyor_1 = NA_character_,
+#                 surveyor_2 = NA_character_,
+#                 surveyor_3 = NA_character_,
+#                 camera_id = NA_character_,
+#                 gps_id = NA_character_,
+#                 gps_waypoint_number = NA_character_,
+#                 photo_site_bottom = NA_character_,
+#                 photo_site_top = NA_character_
+#   ) %>%
+#   # populate utm fields with a location so we can use to burn geopackage
+#   mutate(utm_easting = 687879,
+#          utm_northing = 6020659) %>%
+#   # make it a spatial file so we can burn it as a geopackage right into our mergin file of choice
+#   sf::st_as_sf(coords = c("utm_easting", "utm_northing"),
+#                crs = 32600 + utm_zone, remove = F) %>%
+#   # put it into albers so we can use it anywhere
+#   sf::st_transform(crs = 3005) %>%
+#   relocate(date_time_start,
+#            mergin_user,
+#            contains('surveyor'),
+#            contains('name'),
+#            matches('utm')) %>%
+#   relocate(matches('photo'), .after = last_col()) %>%
+#   # burn to test project for now, will burn to dff when finalised
+#   sf::st_write(paste0('../../gis/',
+#                       dir_project,
+#                       '/',
+#                       file_name_site,
+#                       '.gpkg'),
+#                # turned this T now that we have time in name
+#                delete_layer = T)
 
 form_prep2 <- fish_coll %>%
   # add some columns of our own
@@ -107,4 +107,4 @@ form_prep2 <- fish_coll %>%
                # turned this T now that we have time in name
                delete_layer = T)
 
-  # sf::st_write('data/qgis/form_fish_sample.gpkg', delete_layer = T)
+
