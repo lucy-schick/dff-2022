dcb85a0166377f77493211dc1d5427917528da38
almac2022 <al@newgraphenvironment.com>
2023-08-14 12:57:50 -0700

    add some py-cient commands

--- scripts/qgis/mergin_make_project.sh
+++ scripts/qgis/mergin_make_project.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 # create project
 # ---------------
 # with Mergin Maps authentication token string set as --auth-token env variable run
-mergin create newgraph/sern_skeena_2023 --from-dir ~/Projects/gis/sern_skeena_2023
+mergin create newgraph/test_clay_2023 --from-dir ~/Projects/gis/test_clay_2023
 
 # ---------------
 # invite collaborators
@@ -14,7 +14,13 @@ mergin create newgraph/sern_skeena_2023 --from-dir ~/Projects/gis/sern_skeena_20
 mergin share-add newgraph/sern_skeena_2023 newgraph_bute --permissions writer
 mergin share-add newgraph/sern_skeena_2023 MateoW --permissions writer
 mergin share-add newgraph/sern_skeena_2023 snorris --permissions writer
-
+mergin share-add newgraph/sern_skeena_2023 tieasha.pierre --permissions reader
+mergin share-add newgraph/sern_skeena_2023 seanwong --permissions reader
+mergin share-add newgraph/sern_skeena_2023 sern_bgeldart --permissions reader
+mergin share-add newgraph/sern_peace_fwcp_2023 MateoW --permissions writer
+mergin share-add newgraph/sern_peace_fwcp_2023 Jmdegagne --permissions reader
+mergin share-add newgraph/sern_peace_fwcp_2023 sern_bgeldart --permissions reader
+mergin share-add newgraph/moti_jerv_2023 seanwong --permissions reader
 # ---------------
 # sync changes
 # ---------------
