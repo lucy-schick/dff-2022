63525fca9911e8c91278180141b5ce28683fb344
almac2022 <al@newgraphenvironment.com>
2024-01-03 13:23:05 -0800

    remove qgis project creation from background_layers.sh and put in seperate script.  rearrange order so failures from bcdata layers happen before pulling from server.  Add available layers from aws.  Update BCGW_SOURCES to read geojsons in directory vs bcdata.txt file because empty tables removed as per #147

--- scripts/qgis/background_layers.sh
+++ scripts/qgis/background_layers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 
-# define the name of our Q project
-DIRPROJECT='test_20230907a'
 # ---------------
 # extract background info for crossing assessments digital field form projects
 
@@ -10,7 +8,6 @@ DIRPROJECT='test_20230907a'
 # eg
 # test with ./background_layers.sh "'PARS'" &>log.txt
 # ---------------
-
 
 
 # check that watershed group code is provided as argument
@@ -41,6 +38,39 @@ bcdata dump WHSE_BASEMAPPING.FWA_WATERSHED_GROUPS_POLY \
 BOUNDS=$(fio info background_layers.gpkg --layer fwa_watershed_groups_poly --bounds)
 BOUNDS_LL=$(echo "[$BOUNDS]" | tr ' ', ',' | rio transform --src_crs EPSG:3005 --dst_crs EPSG:4326 | tr -d '[] ')
 
+# ---------------
+# other BCGW/WFS sources
+# download these to geojson first with bcdata.sh
+# ---------------
+# BCGW_SOURCES=$(cat bcdata.txt)
+
+# get the name of the files (except aoi.geojson) that are left after tiny ones are removed
+BCGW_SOURCES=$(find . -maxdepth 1 -type f -name "*.geojson" -a '!' -name "aoi.geojson" -exec basename {} .geojson \;)
+
+
+for layer in $BCGW_SOURCES; do
+    if [[ $layer == *".fwa_"* ]]; then
+        ogr2ogr -f GPKG \
+            background_layers.gpkg \
+            -update \
+            -t_srs EPSG:3005 \
+            -nln $layer \
+            -dim XY \
+            -where "watershed_group_code in ($1)" \
+            $layer.geojson
+    else
+        ogr2ogr -f GPKG \
+            background_layers.gpkg \
+            -update \
+            -t_srs EPSG:3005 \
+            -nln $layer \
+            -dim XY \
+            -clipsrc aoi.geojson \
+            -clipsrclayer aoi \
+            $layer.geojson
+    fi
+done
+
 
 # ---------------
 # lateral habitat
@@ -60,14 +90,17 @@ rm aoi_alb.geojson
 # bcfishpass releated data sources archived to flatgeobuf on s3 for fast retrieval
 # (and with watershed group code included, no spatial query for clipping required)
 # ---------------
-echo 'Getting bcfishpass and supporting layers from s3'
+echo 'Getting bcfishpass and supporting fdb layers from s3'
 FGB_SOURCES="
     crossings \
     fiss_fish_obsrvtn_events_vw \
     fiss_obstacles_pnt_sp \
+    ften_range_poly_carto_vw \
     ften_road_section_lines_svw \
+    pmbc_parcel_fabric_poly_svw \
     streams \
-    transport_line"
+    transport_line \
+    veg_comp_lyr_r1_poly"
 for layer in $FGB_SOURCES; do
   ogr2ogr \
     -f GPKG background_layers.gpkg \
@@ -98,44 +131,5 @@ ogr2ogr -f GPKG background_layers.gpkg \
 
 
 
-# ---------------
-# other BCGW/WFS sources
-# download these to geojson first with bcdata.sh
-# ---------------
-BCGW_SOURCES=$(cat bcdata.txt)
-for layer in $BCGW_SOURCES; do
-    if [[ $layer == *".fwa_"* ]]; then
-        ogr2ogr -f GPKG \
-            background_layers.gpkg \
-            -update \
-            -t_srs EPSG:3005 \
-            -nln $layer \
-            -dim XY \
-            -where "watershed_group_code in ($1)" \
-            $layer.geojson
-    else
-        ogr2ogr -f GPKG \
-            background_layers.gpkg \
-            -update \
-            -t_srs EPSG:3005 \
-            -nln $layer \
-            -dim XY \
-            -clipsrc aoi.geojson \
-            -clipsrclayer aoi \
-            $layer.geojson
-    fi
-done
 
-# ---------------
-# create directory for project and move the files in and copy in a qlr template (works relative to location dragged from when we reference gpkg) along with the directories for info ignored on phones
-# ---------------
-mkdir -p ~/Projects/gis/$DIRPROJECT
-mv background_layers.gpkg* ~/Projects/gis/$DIRPROJECT/
-mv habitat_lateral.tif ~/Projects/gis/$DIRPROJECT/
-cp ../../data/qgis/bcfishpass_mobile.qgs ~/Projects/gis/$DIRPROJECT/$DIRPROJECT.qgs
-cp ../../data/qgis/form_pscis.gpkg ~/Projects/gis/$DIRPROJECT/
-cp ../../data/qgis/form_fiss_site.gpkg ~/Projects/gis/$DIRPROJECT/
-mkdir -p ~/Projects/gis/$DIRPROJECT/ignore_mobile/photos
-touch ~/Projects/gis/$DIRPROJECT/ignore_mobile/photos/photos.txt
-cp -R ../../data/methods ~/Projects/gis/$DIRPROJECT/ignore_mobile/methods
-cp -R ../../data/templates ~/Projects/gis/$DIRPROJECT/ignore_mobile/templates
+
---
+++ scripts/qgis/qgis_create.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euxo pipefail
+
+
+# check that the name of the new directory for the project is provided as argument
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied - provide name of new project directory to be placed in ~Projects/gis/"
+    exit 1
+fi
+
+echo 'Generating QGIS project in Projects/gis/: '$1
+
+
+# define the name of our Q project
+# DIRPROJECT='test_peace'
+
+# ---------------
+# create directory for project and move the files in and copy in a qlr template (works relative to location dragged from when we reference gpkg) along with the directories for info ignored on phones
+
+mkdir ~/Projects/gis/$1
+mv background_layers.gpkg* ~/Projects/gis/$1/
+mv habitat_lateral.tif ~/Projects/gis/$1/
+cp ../../data/qgis/bcfishpass_mobile.qgs ~/Projects/gis/$1/$1.qgs
+cp ../../data/qgis/form_pscis.gpkg ~/Projects/gis/$1/
+cp ../../data/qgis/form_fiss_site.gpkg ~/Projects/gis/$1/
+mkdir -p ~/Projects/gis/$1/ignore_mobile/photos
+touch ~/Projects/gis/$1/ignore_mobile/photos/photos.txt
+cp -R ../../data/methods ~/Projects/gis/$1/ignore_mobile/methods
+cp -R ../../data/templates ~/Projects/gis/$1/ignore_mobile/templates
