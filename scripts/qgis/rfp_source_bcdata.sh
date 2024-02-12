b549edbdba1d5335ae2f0982851efb6957fb7f5e
almac2022 <al@newgraphenvironment.com>
2024-02-12 09:44:23 -0800

    add time stamped tracking file.  update GPKG variable name.  fix issue where $2 (update) not set throws error

--- scripts/qgis/rfp_source_bcdata.sh
+++ scripts/qgis/rfp_source_bcdata.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 set -euxo pipefail
 
+
 # Check that watershed group code is provided as an argument
 if [ $# -eq 0 ]; then
     echo "No arguments supplied - provide list of watershed_group_code values for watersheds of interest."
     exit 1
 fi
 
+
+# Temporarily disable the nounset option
+set +u
+
+# Assign a default value to $2 if it's not set
+operation=${2:-"generate"}
+
+# Re-enable the nounset option
+set -u
+
+
+
+# clean out the old files
+rm -f *.geojson
+rm -f *.tif
+
+
 # Name of the GeoPackage file
-FILE_REMOVE="background_layers.gpkg"
+GPKG="background_layers.gpkg"
+
+# name the sources file
+SOURCES="rfp_source_bcdata.txt"
+
 
 # Check for "update" flag and existence of the GeoPackage file
-if [[ "$2" == "update" && ! -f "$FILE_REMOVE" ]]; then
-    echo "$FILE_REMOVE does not exist. Cannot perform update operation."
+if [[ "$operation" == "update" && ! -f "$GPKG" ]]; then
+    echo "$GPKG does not exist. Cannot perform update operation."
     exit 1
 fi
+
+# # Check for "update" flag and existence of the GeoPackage file
+# if [[ "$2" == "update" && ! -f "$GPKG" ]]; then
+#     echo "$GPKG does not exist. Cannot perform update operation."
+#     exit 1
+# fi
 
 process_geopackage() {
     local watersheds="$1"
@@ -30,7 +58,7 @@ process_geopackage() {
         update_option="-update"
     fi
 
-    ogr2ogr -f GPKG "$FILE_REMOVE" \
+    ogr2ogr -f GPKG "$GPKG" \
             $update_option \
             -overwrite \
             -t_srs EPSG:3005 \
@@ -39,29 +67,26 @@ process_geopackage() {
 }
 
 # Main script logic
-if [[ -f "$FILE_REMOVE" ]]; then
-    read -p "Do you wish to start over with a new $FILE_REMOVE? If you are updating existing project info say NO (y/n): " answer
+if [[ -f "$GPKG" ]]; then
+    read -p "Do you wish to start over with a new $GPKG? If you are updating existing project info say NO (y/n): " answer
     if [[ $answer == "y" ]]; then
-        echo "Removing $FILE_REMOVE..."
-        rm -f "$FILE_REMOVE"
+        echo "Removing $GPKG..."
+        rm -f "$GPKG"
         process_geopackage "$1"
     else
-        echo "Updating existing $FILE_REMOVE with area of interest..."
+        echo "Updating existing $GPKG with area of interest..."
         process_geopackage "$1" "update"
     fi
 else
-    if [[ "$2" == "update" ]]; then
+    if [[ "$operation" == "update" ]]; then
         # This block should never be reached due to the initial check, but it's here for logical completeness
-        echo "Cannot perform update because $FILE_REMOVE is missing."
+        echo "Cannot perform update because $GPKG is missing."
         exit 1
     else
         echo "Generating a new one..."
         process_geopackage "$1"
     fi
 fi
-
-
-
 
 
 # get bounding box of aoi in BC Albers and WGS84 (lon/lat)
@@ -73,7 +98,7 @@ BOUNDS=$(echo "[$BOUNDS_LL]" | tr ' ', ',' | rio transform --src_crs EPSG:4326 -
 # non-fwa bcgw layers
 # ---------------
 echo 'Getting BC Data Catalogue layers - this may take a while'
-BCGW_SOURCES=$(grep -v '^#' rfp_source_bcdata.txt)
+BCGW_SOURCES=$(grep -v '^#' $SOURCES)
 for layer in $BCGW_SOURCES; do
     if [ ! -f ./$layer.geojson ]; then
         set -e ; bcdata dump $layer --bounds "$BOUNDS" --bounds-crs EPSG:3005 -l > $layer.geojson
@@ -113,4 +138,24 @@ for layer in $BCGW_SOURCES_CLEANED; do
     fi
 done
 
+echo 'Creating a record of the layers that were loaded'
+# create a record of the layers that were loaded
+# Get the current date and time
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M')
+
+# Header for the CSV file
+echo "timestamp,content,watershed_groups,source" > temp.csv
+
+# Use awk to process the text file line by line
+awk -v timestamp="$TIMESTAMP" -v watershed_groups="$1" -v source="$SOURCES" '!/^#/ && NF > 0 {print timestamp "," $0 "," watershed_groups "," source}' $SOURCES >> temp.csv
+
+# Append the temporary CSV file to the GeoPackage
+ogr2ogr -append -f "GPKG" $GPKG temp.csv -nln rfp_tracking
+
+# Remove the temporary CSV file
+rm temp.csv
+
+
 echo 'bcdata layers are now loaded to background_layers.gpkg'
+
+
