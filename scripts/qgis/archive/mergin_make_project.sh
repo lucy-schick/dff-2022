d54099e528c523f5d95459cb761bf25fe3e2c7e8
almac2022 <al@newgraphenvironment.com>
2024-10-03 10:41:42 -0700

    sern_fraser_2024 custom run

--- scripts/qgis/archive/mergin_make_project.sh
+++ scripts/qgis/archive/mergin_make_project.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 set -euxo pipefail
 
+# 20241003 run in dff2 virtual environment
+
+# sern_fraser_2024
+# ./rfp_source_bcdata.sh "'LCHL', 'NECR', 'FRAN', 'MORK', 'UFRA', 'TABR', 'WILL'" && ./rfp_source_aws.sh "'LCHL', 'NECR', 'FRAN', 'MORK', 'UFRA', 'TABR', 'WILL'" && time ./rfp_qgis_create.sh "sern_fraser_2024" "bcrestoration_mobile.qgs"
+
 # ---------------
 # create project
 # ---------------
 # with Mergin Maps authentication token string set as --auth-token env variable run
-mergin create newgraph/test_cano_2023 --from-dir ~/Projects/gis/test_cano_2023
+mergin create newgraph/sern_fraser_2024 --from-dir ~/Projects/gis/sern_fraser_2024
 
 # ---------------
 # invite collaborators
 # ---------------
 # executed from project directory with .mergin file present
-mergin share-add newgraph/sern_skeena_2023 newgraph_bute --permissions writer
-mergin share-add newgraph/sern_skeena_2023 MateoW --permissions writer
-mergin share-add newgraph/sern_skeena_2023 snorris --permissions writer
-mergin share-add newgraph/sern_skeena_2023 tieasha.pierre --permissions reader
-mergin share-add newgraph/sern_skeena_2023 seanwong --permissions reader
-mergin share-add newgraph/sern_skeena_2023 sern_bgeldart --permissions reader
-mergin share-add newgraph/sern_peace_fwcp_2023 MateoW --permissions writer
-mergin share-add newgraph/sern_peace_fwcp_2023 Jmdegagne --permissions reader
-mergin share-add newgraph/sern_peace_fwcp_2023 sern_bgeldart --permissions reader
-mergin share-add newgraph/moti_jerv_2023 seanwong --permissions reader
+mergin share-add newgraph/sern_fraser_2024 newgraph_bute --permissions writer
+mergin share-add newgraph/sern_fraser_2024 newgraph_lschick --permissions writer
+mergin share-add newgraph/sern_fraser_2024 snorris --permissions writer
+mergin share-add newgraph/sern_fraser_2024 seanwong --permissions writer
+mergin share-add newgraph/sern_fraser_2024 sern_bgeldart --permissions writer
+mergin share-add newgraph/sern_fraser_2024 Jmdegagne --permissions writer
+mergin share-add newgraph/sern_fraser_2024 Alexandras --permissions writer
+mergin share-add newgraph/sern_fraser_2024 tdonald --permissions writer
+mergin share-add newgraph/sern_fraser_2024 rivershed_jmarquis --permissions writer
+mergin share-add newgraph/sern_fraser_2024 Mya Eastmure --permissions writer
 # ---------------
 # sync changes
 # ---------------
 # executed from project directory with .mergin file present
-mergin status newgraph/sern_skeena_2023
-mergin push newgraph/sern_skeena_2023
+# mergin status newgraph/sern_fraser_2024
+# mergin push newgraph/sern_fraser_2024
 
 # ---------------
 # clone a project
