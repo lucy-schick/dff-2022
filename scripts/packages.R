97b1b7506aa1ac0afc1414742a380d0dc98d1501
newgraph-lschick <lucy@newgraphenvironment.com>
2025-08-29 15:13:22 -0700

    update packages script to use pak. Make package updates optional

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,3 +1,21 @@
+# Choose if you would like to update packages or not
+update_packages <- FALSE
+
+
+
+# ensure pak is installed and up to date from CRAN
+if (!requireNamespace("pak", quietly = TRUE)) {
+  install.packages("pak")
+} else {
+  # Only run this if an update is needed
+  current <- packageVersion("pak")
+  latest <- package_version(available.packages()["pak", "Version"])
+  if (current < latest) {
+    pak::pak("pak")  # uses pak to update itself = no popup
+  }
+}
+
+
 # # install.packages('pacman')
 #
 # pacman::p_load(
@@ -40,7 +58,7 @@
 # install.packages('pacman')
 
 
-package_list <- c(
+pkgs_cran <- c(
   # plotKML,  #plot kml needed to go ahead of other packages for some reason and wants to reinstall everytime.... not sure why. hash out for now
   'tidyverse',
   'sf',
@@ -61,20 +79,37 @@ package_list <- c(
   'forcats',
   'bookdown',
   'geojsonio',
-  'xlsx',
-  'fpr'
+  'xlsx'
 )
-lapply(package_list,
-       require,
-       character.only = TRUE)
 
 # we need the development version of pagedown as of 20200303 https://github.com/rstudio/pagedown/issues/265
 # remotes::install_github('rstudio/pagedown')
 
+pkgs_gh <- c(
+  "newgraphenvironment/fpr",
+  "poissonconsulting/fwapgr",
+  "poissonconsulting/poisspatial",
+  "lucy-schick/fishbc@updated_data",
+  "crsh/citr",
+  "rstudio/pagedown"
+  # "poissonconsulting/subfoldr2")
+)
 
-pacman::p_load_gh("poissonconsulting/fwapgr",
-                  'poissonconsulting/poisspatial',
-                  "crsh/citr",
-                  'rstudio/pagedown',
-                  "poissonconsulting/fishbc")
-# "poissonconsulting/subfoldr2")
+
+pkgs_all <- c(pkgs_cran,
+              pkgs_gh)
+
+# install or upgrade all the packages with pak
+if(update_packages){
+  lapply(pkgs_all, pak::pkg_install, ask = FALSE)
+}
+
+# load all the packages
+pkgs_ld <- c(pkgs_cran,
+             basename(pkgs_gh))
+
+lapply(pkgs_ld,
+       require,
+       character.only = TRUE)
+
+
